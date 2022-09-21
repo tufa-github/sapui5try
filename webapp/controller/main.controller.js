4c9f6f213b2646d3003414f185e7b218dcd7a2b8
Zheng Wu <zhengw1@verifone.com>
2022-09-21 16:57:23 +0200

    step 8 Mui-language

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -1,12 +1,37 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 	"use strict";
 
 	return Controller.extend("tufa.ui5.try.controller.main", {
+		
+		onInit: function(){
+			var myData = {
+				person:{
+					name:"tufa",
+					age:16
+				}
+			};
+			var oModel = new JSONModel(myData);
+			this.getView().setModel(oModel);
+
+			var i18nModel = new ResourceModel({
+				bundleName:"tufa.ui5.try.i18n.i18n"
+
+			});
+			this.getView().setModel(i18nModel, "tufai18n");
+		},
+
 		onPress : function () {
-			MessageToast.show("Hello App!");
+			//MessageToast.show("Hello App!");
+
+			var oBundle = this.getView().getModel("tufai18n").getResourceBundle();
+			var sPerson = this.getView().getModel().getProperty("/person/name");
+
+			MessageToast.show(oBundle.getText("helloMsg", [sPerson]));
 		}
 	});
 
